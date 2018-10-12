e6baa0638d880ae741eea5df46fb1b77d0487185
Eshan-k <eshan.kulkarni15@gmail.com>
2018-10-11 23:41:25 -0400

    feature: created content.blade.php

---
+++ resources/views/layout/content.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	@include('includes.head')
+</head>
+<body>
+<div class="container">
+	@include('includes.header')
+	<main role="main">
+	
+	</main>
+</div>
+<footer class="container-fluid bg-dark text-white text-center">
+	<div class="container">
+	
+	</div>
+</footer>
+@include('includes.jsfile')
+</body>
+</html>
