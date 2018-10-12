a5af51b0697e9704e4c80de7bada9472d5575ffd
Eshan-k <eshan.kulkarni15@gmail.com>
2018-10-11 23:47:21 -0400

    fix: edited home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.content')
 @section('content')
-	{{--<div class="form-group">--}}
-	<h1>This is Home Page</h1>
-	{{--</div>--}}
+	<div class="form-group">
+		<h1 class="text-secondary">This is Home Page</h1>
+	</div>
 @stop
