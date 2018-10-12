a548a95c5313c7f1b8aaf78a92e0c660e5b454c8
Eshan-k <eshan.kulkarni15@gmail.com>
2018-10-11 23:48:44 -0400

    fix: edited about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,6 @@
 @extends('layout.content')
 @section('content')
-	{{--<div class="form-group">--}}
-	<h1>This is About Page</h1>
+	<div class="form-group">
+		<h1 class="text-secondary">This is About Page</h1>
+	</div>
 @stop
